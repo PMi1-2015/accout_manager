8c60e1be88e1e1f9a20dd5398f830babe79f2467
helgi <oleggio.kir@gmail.com>
2017-12-10 14:04:47 +0200

    [projects] Added ability to update/delete project

--- AccountSystem.Services/IProjectService.cs
+++ AccountSystem.Services/IProjectService.cs
@@ -10,6 +10,11 @@ namespace AccountSystem.Services
     public interface IProjectService
     {
         void SaveProject(Project project);
+
+        void UpdateProject(Project project);
+
+        void DeleteProject(Project project);
+
         List<Project> SearchProject(String name, DateTime? startDate, DateTime? endDate);
     }
 }
---
+++ AccountSystem/ViewModels/ProjectViewModel.cs
@@ -0,0 +1,55 @@
+﻿using AccountSystem.Data.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountSystem.ViewModels
+{
+    class ProjectViewModel: BaseViewModel
+    {
+        public ProjectViewModel()
+        {
+
+        }
+
+        public ProjectViewModel(Project project)
+        {
+            _projectName = project.Name;
+            _projecDescription = project.Description;
+            _startDate = project.StartDate;
+            _endDate = project.EndDate;
+        }
+
+
+        private String _projectName;
+        public String ProjectName
+        {
+            get { return _projectName; }
+            set { _projectName = value; OnPoropertyChanged("ProjectName"); }
+        }
+
+        private String _projecDescription;
+        public String ProjectDescription
+        {
+            get { return _projecDescription; }
+            set { _projecDescription = value; OnPoropertyChanged("ProjectDescription"); }
+        }
+
+        private DateTime _startDate;
+        public DateTime StartDate
+        {
+            get { return _startDate; }
+            set { _startDate = value; OnPoropertyChanged("StartDate"); }
+        }
+
+        private DateTime _endDate;
+        public DateTime EndDate
+        {
+            get { return _endDate; }
+            set { _endDate = value; OnPoropertyChanged("EndDate"); }
+        }
+        
+    }
+}
