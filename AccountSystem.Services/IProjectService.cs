76127a06242aca09a572774089ae4c29befb591e
Bohdan <bohdanpastuschak@gmail.com>
2017-12-11 09:27:50 +0200

    Check all

--- AccountSystem.Services/IProjectService.cs
+++ AccountSystem.Services/IProjectService.cs
@@ -1,9 +1,6 @@
 ﻿using AccountSystem.Data.models;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AccountSystem.Services
 {
