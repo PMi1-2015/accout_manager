92bc14e6fcf7376ca437febbadae2b1a6c6b3200
Marych Oleh <marycholeg@gmail.com>
2017-12-12 22:54:16 +0200

    change shared files (add employees to main logic)

---
+++ AccountSystem.Services/ServiceLocator.cs
@@ -0,0 +1,38 @@
+﻿using AccountSystem.Data;
+using AccountSystem.Services.impl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity;
+
+namespace AccountSystem.Services
+{
+    public class ServiceLocator
+    {
+        private IUnityContainer _container;
+
+        private static ServiceLocator _serviceLocator = new ServiceLocator();
+
+        public static ServiceLocator Instance => _serviceLocator;
+
+        private ServiceLocator()
+        {
+            _container = new UnityContainer();
+            Bind();
+        }
+
+        private void Bind()
+        {
+            _container.RegisterInstance<IAppUserService>(new AppUserService(UnitOfWork.Instance));
+            _container.RegisterInstance<IProjectService>(new ProjectService(UnitOfWork.Instance));
+            _container.RegisterInstance<IEmployeeService>(new EmployeeService(UnitOfWork.Instance));
+        }
+
+        public T GetService<T>()
+        {
+            return _container.Resolve<T>();
+        }
+    }
+}
---
+++ AccountSystem/ViewModels/MenuViewModel.cs
@@ -0,0 +1,35 @@
+﻿using AccountSystem.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace AccountSystem.ViewModels
+{
+    public class MenuViewModel: BaseViewModel
+    {
+        public ICommand ProjectsCommand { get; set; }
+        public ICommand EmployeeCommand { get; set; }
+
+        private NavigationViewModel _navigationViewModel;
+
+        public MenuViewModel(NavigationViewModel navigationViewModel)
+        {
+            _navigationViewModel = navigationViewModel;
+
+            ProjectsCommand = new RelayCommand(GoToProjectsControl);
+            EmployeeCommand = new RelayCommand(GoToEmployeesControl);
+        }
+
+        private void GoToProjectsControl(object obj)
+        {
+            _navigationViewModel.SelectedViewModel = new ProjectsCrudViewModel(_navigationViewModel);
+        }
+        private void GoToEmployeesControl(object obj)
+        {
+            _navigationViewModel.SelectedViewModel = new EmployeesCrudViewModel(_navigationViewModel);
+        }
+    }
+}
